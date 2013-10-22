ac1e2c4831008a4309142f3bc268c3b873cd594f
sylvrec <sylvrec@gmail.com>
2013-10-22 10:22:21 +0200

    Fix bug - DestroyView correctly

--- LocateMyStickers/src/main/java/com/locatemystickers/MapActivity.java
+++ LocateMyStickers/src/main/java/com/locatemystickers/MapActivity.java
@@ -1,32 +1,21 @@
 package com.locatemystickers;
 
-import android.app.AlertDialog;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.app.Notification;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
 
-import java.util.List;
-
-public class MapActivity extends MapFragment {
+public class MapActivity extends Fragment {
     private ScreenView _sv;
     private GoogleMap _map;
     private SupportMapFragment _mapView;
+    private FragmentTransaction _fragTrans;
 
     static MapActivity newInstance(ScreenView sv) {
         return new MapActivity(sv);
@@ -38,15 +27,27 @@ public class MapActivity extends MapFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (savedInstanceState != null)
+        View v = inflater.inflate(R.layout.map_activity, container);
+        _mapView = ((SupportMapFragment)_sv.getSupportFragmentManager().findFragmentById(R.id.mapView));
+        if (_mapView != null)
         {
-            inflater.inflate(R.layout.map_activity, container);
-            _mapView = ((SupportMapFragment) _sv.getSupportFragmentManager().findFragmentById(R.id.mapView));
             _map = _mapView.getMap();
             _map.setMyLocationEnabled(true);
             _map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
-
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        FragmentTransaction _fragTrans = _sv.getSupportFragmentManager().beginTransaction();
+        _fragTrans.remove(_mapView);
+        _fragTrans.commit();
     }
 }
