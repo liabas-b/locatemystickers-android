44d4860dca63273c1ac01cc7546f4c2dfb97512a
reclus_s <reclus_s@epitech.eu>
2014-01-29 17:42:29 +0100

    Bug fixed for the mapactivity Youppiii

--- locatemystickers/src/main/java/com/locatemystickers/MapActivity.java
+++ locatemystickers/src/main/java/com/locatemystickers/MapActivity.java
@@ -1,8 +1,10 @@
 package com.locatemystickers;
 
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +13,16 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.locatemystickers.menu.MenuActivity;
 
 public class MapActivity extends Fragment {
 
+    private final String MAP_TAG = "maptag";
+
     private MenuActivity _context;
+    private MapFragment _mapFragment;
+    GoogleMap _googleMap;
 
     public static Fragment newInstance(MenuActivity context) {
         return new MapActivity(context);
@@ -29,9 +36,23 @@ public class MapActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.map_activity, container, false);
         if (savedInstanceState == null) {
-            SupportMapFragment mapView = (SupportMapFragment)_context.getSupportFragmentManager().findFragmentById(R.id.mapView);
-            GoogleMap googleMap = mapView.getMap();
+            _mapFragment = MapFragment.newInstance();
+            FragmentTransaction transaction = _context.getFragmentManager().beginTransaction();
+            transaction.add(R.id.mapfrag, _mapFragment, MAP_TAG);
+            transaction.commit();
+        } else {
+            _mapFragment = (MapFragment)_context.getFragmentManager().findFragmentByTag(MAP_TAG);
+
         }
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((_googleMap = _mapFragment.getMap()) != null)
+            _googleMap.setMyLocationEnabled(true);
+
     }
 }
